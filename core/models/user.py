dfbf64836db45f0e6273228a2e67dc536732301f
Ainero <aineroprogramming@gmail.com>
2025-08-01 02:20:51 +0300

    Create alembic migration, for products table, fix error.

--- core/models/user.py
+++ core/models/user.py
@@ -1,10 +1,10 @@
-
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 from .base import Base
 
+
 class User(Base):
-    
-    username: Mapped[str] = mapped_column(String(32)unique=True)
+
+    username: Mapped[str] = mapped_column(String(32), unique=True)
     description: Mapped[str]
     price: Mapped[int]
